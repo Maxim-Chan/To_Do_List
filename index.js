1f665df1672d01b34e63533709aa0b11e998f77b
Maxim-Chan <vi0leten77@gmail.com>
2024-08-04 21:55:43 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+import express from "express";
+import pg from "pg"
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// let items = [
+//   { id: 1, title: "Buy milk" },
+//   { id: 2, title: "Finish homework" },
+// ];
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "to_do_list",
+  password: "",
+  port: 5432
+})
+
+db.connect()
+
+app.get("/", async (req, res) => {
+  try{
+    const result = await db.query("SELECT * FROM items");
+    const items = result.rows;
+  
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: items,
+    });
+  } catch (err){
+    console.log(err)
+  }
+});
+
+app.post("/add", async (req, res) => {
+  try{
+    const item = req.body.newItem;
+    await db.query("INSERT INTO items(title) VALUES ($1)", [item])
+
+  } catch (err){
+    console.log(err);
+  }
+  res.redirect("/");
+});
+
+app.post("/edit", (req, res) => {});
+
+app.post("/delete", (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
